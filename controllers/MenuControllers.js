a8d68a12d24705c3f0c9ae863556603a2e6be350
Kyle D <donovanky@my.easternct.edu>
2019-06-28 20:49:41 -0400

    Added date and contact start

---
+++ controllers/MenuControllers.js
@@ -0,0 +1,60 @@
+const inquirer = require('inquirer');
+
+module.exports = class MenuController {
+  constructor(){
+    this.mainMenuQuestions = [
+      {
+        type: "list",
+        name: "mainMenuChoice",
+        message: "Please choose from an option below: ",
+        choices: [
+          "Add new contact",
+          "Get date",
+          "Exit"
+        ]
+      }
+    ];
+    this.contacts = [];
+  }
+  main(){
+    console.log(`Welcome to AddressBloc!`);
+     inquirer.prompt(this.mainMenuQuestions).then((response) => {
+       switch(response.mainMenuChoice){
+         case "Add new contact":
+           this.addContact();
+           break;
+         case "Get date":
+           this.getDate();
+            break;
+         case "Exit":
+           this.exit();
+         default:
+           console.log("Invalid input");
+           this.main();
+       }
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+  }
+  clear(){
+    console.log("\x1Bc");
+  }
+  addContact(){
+    this.clear();
+    console.log('addContact called');
+    this.main();
+  }
+  getDate(){
+    this.clear();
+    var date = new Date();
+    console.log('The date is: ' + date);
+    this.main();
+
+  }
+  exit(){
+    console.log("Thanks for using AddressBloc!");
+    process.exit();
+  }
+
+}
